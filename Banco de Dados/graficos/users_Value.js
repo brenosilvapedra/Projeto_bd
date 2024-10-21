9e00e246592dcd0a2f9c40d2401de357c3e17a6f
brenosilvapedra <112491458+brenosilvapedra@users.noreply.github.com>
2024-10-21 11:06:39 -0300

    Add files via upload

--- Banco de Dados/graficos/users_Value.js
+++ Banco de Dados/graficos/users_Value.js
@@ -4,5 +4,28 @@ import { getCSS } from './comum';
 async function users_network() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json';
     const res = await fetch(url);
-    const dados = await res.json()
+    const dados = await res.json();
+    const nome_Redes = Object.values(dados);
+    const quantidade_Users = Object.values(dados);
+
+    const data = [
+        {
+            x: nome_Redes,
+            y: quantidade_Users,
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ];
+
+    const layout = {
+        plot_bgColor: getCSS('--bg-color'),
+        paper_bgColor: getCSS('--bg-color'),
+    };
+
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('grafico-container').appendChild(grafico);
+    Plotly.newPlot(grafico, data, layout)
 }
